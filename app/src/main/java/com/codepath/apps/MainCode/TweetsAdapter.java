541ca520ea4b1ef603fafadf2e98461d195680bf
Jack Walter Yurkanin <yurkanin@fb.com>
2021-06-30 13:52:32 -0700

    Added refresh option

--- app/src/main/java/com/codepath/apps/MainCode/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/MainCode/TweetsAdapter.java
@@ -27,6 +27,18 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         this.tweets = tweets;
     }
 
+    // Clean all elements of the recycler
+    public void clear() {
+        tweets.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+    public void addAll(List<Tweet> list) {
+        tweets.addAll(list);
+        notifyDataSetChanged();
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
